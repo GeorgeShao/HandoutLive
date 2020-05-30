36372bb1c72bbf3036f746f02a6464fd934d4202
Leon Si <leonzalion@gmail.com>
2020-05-30 14:38:33 -0400

    hi

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <v-app>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
     <router-view />
   </v-app>
 </template>
---
+++ src/pages/TeacherPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card height="350px">
+    <v-navigation-drawer
+      absolute
+      permanent
+      right
+    >
+      <template v-slot:prepend>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Jane Smith</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+
+      <v-divider></v-divider>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'TeacherPage'
+}
+</script>
+
+<style>
+</style>
