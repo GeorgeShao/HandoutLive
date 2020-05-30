5bbadac02f8c4936ad99ac5593fd5d1c900489b0
Leon Si <leonzalion@gmail.com>
2020-05-30 18:17:42 -0400

    hi

--- src/pages/TeacherPage.vue
+++ src/pages/TeacherPage.vue
@@ -33,58 +33,60 @@
     </v-navigation-drawer>
 
     <v-content>
-      <DrawingCanvas />
-      <v-card width="200px">
-        <v-card
-          class="mx-auto"
-          max-width="400"
-          tile
-        >
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>John Doe</v-list-item-title>
-              <v-list-item-subtitle>How do you do question #3?</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+      <div class="wrapper">
+        <DrawingCanvas />
+        <v-sheet>
+          <v-card
+            class="mx-auto"
+            max-width="400"
+            height="100%"
+            tile
+          >
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>John Doe</v-list-item-title>
+                <v-list-item-subtitle>How do you do question #3?</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
 
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>Jane Smith</v-list-item-title>
-              <v-list-item-subtitle>I don't understand question #5</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>Jane Smith</v-list-item-title>
+                <v-list-item-subtitle>I don't understand question #5</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
 
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>Tommy Chen</v-list-item-title>
-              <v-list-item-subtitle>Can you show me how to do #7?</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title>Tommy Chen</v-list-item-title>
+                <v-list-item-subtitle>Can you show me how to do #7?</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
 
-        </v-card>
+            <v-spacer />
 
-        <v-divider></v-divider>
+            <v-text-field
+              label="Solo"
+              placeholder="Placeholder"
+              solo
+            ></v-text-field>
+          </v-card>
+        </v-sheet>
+      </div>
 
-        <v-text-field
-          label="Solo"
-          placeholder="Placeholder"
-          solo
-        ></v-text-field>
-      </v-card>
-    </v-content>
-  </v-app>
-      <!--
       <v-btn
         absolute
         dark
         fab
         bottom
         left
+        style="margin-bottom: 40px"
         color="blue"
       >
         <v-icon>mdi-upload</v-icon>
       </v-btn>
-      -->
+    </v-content>
+  </v-app>
 </template>
 <script>
   import DrawingCanvas from '../components/DrawingCanvas';
@@ -100,12 +102,9 @@
 </script>
 
 <style>
-  .v-content {
+  .wrapper {
     display: flex;
-    flex-direction: row;
-  }
-
-  .canvas {
-    flex-grow: 1;
+    height: 100%;
+    overflow: hidden;
   }
 </style>
