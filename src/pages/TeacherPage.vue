f69589b5a8492d62caaefee27d271a4e14a5ef45
Leon Si <leonzalion@gmail.com>
2020-05-30 17:19:58 -0400

    hi

--- src/pages/TeacherPage.vue
+++ src/pages/TeacherPage.vue
@@ -13,27 +13,7 @@
           v-model="studentDrawer"
           absolute
         >
-          <template v-slot:prepend>
-            <v-list-item two-line>
-              <v-list-item-avatar>
-                <img src="https://randomuser.me/api/portraits/women/81.jpg">
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>Jane Smith</v-list-item-title>
-                <v-list-item-subtitle>Logged In</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-
-          <v-divider></v-divider>
-        </v-navigation-drawer>
-
-        <v-sheet class="canvas">
-          <DrawingCanvas />
-        </v-sheet>
-
-        <v-card width="200px">
+          <v-divider />
           <template v-slot:prepend>
             <v-list-item two-line>
               <v-list-item-content>
@@ -43,7 +23,26 @@
             </v-list-item>
           </template>
 
-          <v-divider></v-divider>
+          <v-divider />
+
+          <v-list-item two-line>
+            <v-list-item-avatar>
+              <img src="https://randomuser.me/api/portraits/women/81.jpg">
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Jane Smith</v-list-item-title>
+              <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-navigation-drawer>
+
+        <v-sheet class="canvas">
+          <DrawingCanvas />
+        </v-sheet>
+
+        <v-card width="200px">
+
 
           <v-card
             class="mx-auto"
