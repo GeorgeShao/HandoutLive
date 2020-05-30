376c821c33bce46d0855f72189d360cf711d1ca0
George Shao <GeorgeShao123@gmail.com>
2020-05-30 14:37:57 -0400

    Added room code login page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <v-app>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link> -->
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Welcome!</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Room Code" prepend-icon="mdi-lock"/>
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="success">Create</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Join</v-btn>
+      </v-card-actions>
+    </v-card>
     <router-view />
   </v-app>
 </template>
